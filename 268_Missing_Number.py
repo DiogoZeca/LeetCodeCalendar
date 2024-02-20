e88666dfd64857cf5ffdb5e87d7d7aa0b1d64353
Diogo Silva <93706766+DiogoZeca@users.noreply.github.com>
2024-02-20 22:00:25 +0000

    20 February 2024

--- 268_Missing_Number.py
+++ 268_Missing_Number.py
@@ -5,7 +5,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        expected_sum = n*(n+1)//2
+        expected_sum = n*(n+1)//2                       #formula to calculate sum of n natural numbers
         actual_sum = sum(nums)
         return expected_sum - actual_sum
     
