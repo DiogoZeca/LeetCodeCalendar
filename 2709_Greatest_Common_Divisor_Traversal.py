9f55efab9163b7531a9ecb68fcc3f23cdff6e077
Diogo Silva <93706766+DiogoZeca@users.noreply.github.com>
2024-02-25 20:49:57 +0000

    25 February 2024

---
+++ 2709_Greatest_Common_Divisor_Traversal.py
@@ -0,0 +1,24 @@
+# You are given a 0-indexed integer array nums, and you are allowed to traverse between its indices.
+# You can traverse between index i and index j, i != j, if and only if gcd(nums[i], nums[j]) > 1, where gcd is the greatest common divisor.
+# Your task is to determine if for every pair of indices i and j in nums, where i < j, there exists a sequence of traversals that can take us from i to j.
+# Return true if it is possible to traverse between all such pairs of indices, or false otherwise.
+
+from math import gcd
+
+class Solution:
+    def canTraverseAllPairs(self, nums: List[int]) -> bool:
+        if len(nums)==1:return True
+        nums = set(nums)
+        if 1 in nums:return False
+        if len(nums)==1:return True
+        
+        nums = sorted(nums,reverse=True)
+
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if gcd(nums[i],nums[j])-1:
+                    nums[j]*=nums[i]
+                    break
+            else:
+                return False
+        return True
