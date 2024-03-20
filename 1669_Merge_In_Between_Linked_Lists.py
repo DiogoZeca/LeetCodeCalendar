b31c2c559a495707c1c39b5f3b9b49ccbc9e9c01
Diogo Silva <93706766+DiogoZeca@users.noreply.github.com>
2024-03-20 10:49:53 +0000

    20 March 2024

---
+++ 1669_Merge_In_Between_Linked_Lists.py
@@ -0,0 +1,26 @@
+# You are given two linked lists: list1 and list2 of sizes n and m respectively.
+# Remove list1's nodes from the ath node to the bth node, and put list2 in their place.
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
+        ptr = list1
+        for _ in range(a - 1):
+            ptr = ptr.next
+        
+        qtr = ptr.next
+        for _ in range(b - a + 1):
+            qtr = qtr.next
+        
+        ptr.next = list2
+        while list2.next:
+            list2 = list2.next
+        list2.next = qtr
+        
+        return list1
+
